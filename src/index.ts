45b6d7287df495c4c85455006fd53f4105e92bb5
Amanda <amandchen@hotmail.com>
2024-08-01 00:15:50 -0300

    fix: index routes

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,31 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import Fastify from "fastify";
+import Fastify, { FastifyRequest } from "fastify";
 import { mongoConnect, mongoDisconnect } from "./database/mongoose-connect";
-// import { routes } from "./routes";
+import { userRoutes } from "./modules/users/routes/user.route";
+import { postRoutes } from "./modules/posts/routes/post.route";
+import { ratingRoutes } from "./modules/ratings/routes/rating.route";
 
 const fastify = Fastify({ logger: true });
 
-// Conectar ao MongoDB
 mongoConnect();
 
-// Registrar rotas
-// fastify.register(routes);
+fastify.register(userRoutes, { prefix: "/api/users" });
+fastify.register(postRoutes, { prefix: "/api/posts" });
+fastify.register(ratingRoutes, { prefix: "/api/ratings" });
 
 fastify.get("/", async (request, reply) => {
   return { hello: "world" };
 });
 
+// server start function
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000, host: "0.0.0.0" });
+    await fastify.listen({
+      port: Number(process.env.PORT) || 3000,
+      host: "0.0.0.0",
+    });
     console.log(
       `Server running on http://localhost:${process.env.PORT || 3000}`
     );
@@ -30,8 +36,10 @@ const start = async () => {
   }
 };
 
+// server start
 start();
 
+// graceful shutdown
 process.on("SIGINT", async () => {
   console.log("SIGINT signal received: closing MongoDB connection");
   await mongoDisconnect();
